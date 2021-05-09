99c34d603a790047e8efa71894ca3c2005a55d1f
NarutoCoder <81152878+NarutoCoder@users.noreply.github.com>
2021-05-10 00:19:51 +0530

    Create NumberPattern.cpp

---
+++ C++/Fundamentals/NumberPattern.cpp
@@ -0,0 +1,20 @@
+int main(){
+  
+    int n ;
+    cin>>n;
+     int x;
+    for(int i =1;i<=n;i++){
+        x = i;
+        for(int j=0;j<i;j++){
+            
+           cout<<x;
+            ++x;
+            
+        }
+        
+        cout<<endl;
+        
+        
+    }
+  
+}
